0ddea06cfec92ddfadb50c6c3cc9452479586ab3
Ngadohang <ngado29895@gmail.com>
2018-05-24 15:18:42 +0700

    aqbc

---
+++ myProjectFisrt/1.js
@@ -0,0 +1,29 @@
+var time =1500;
+var random_display=0;
+var imageDir ="img/";
+var imgaeNum=0;
+imageArray= new Array();
+imageArray[imageNum++]= new imageItem(imageDir+"")
+imageArray[imageNum++]= new imageItem(imageDir+"")
+imageArray[imageNum++]= new imageItem(imageDir+"")
+imageArray[imageNum++]= new imageItem(imageDir+"")
+// tạo function tạo location cho ảnh và fuction lay ảnh
+// <script type="text/javascript">
+// 		window.onload=function(){
+// 			setTimeout("swithImages()",2000);
+// 		}
+// 		var current=1;
+// 		var numberImg=2;
+// 		function swithImages()
+// 		{			
+// 			if (current<numberImg) {
+// 				current++;
+// 				document.images['myimage'].src='images/carousel/banner-'+current+'.jpg';
+// 				setTimeout("swithImages()",3000);
+// 			}else if (current>=numberImg) {
+// 				current--;
+// 				document.images['myimage'].src='images/carousel/banner-'+current+'.jpg';
+// 				setTimeout("swithImages()",3000);
+// 			}
+// 		}
+// 	</script>
